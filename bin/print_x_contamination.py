77ee2a25eea697da2e5768d5fe79c8ce1f5f0069
TCLamnidis <thisseass@gmail.com>
2020-07-13 15:01:17 +0200

    MultiQC custom content style json output

--- bin/print_x_contamination.py
+++ bin/print_x_contamination.py
@@ -2,6 +2,7 @@
 import sys, re, json
 from collections import OrderedDict
 
+jsonOut=OrderedDict()
 data=OrderedDict()
 
 Input_files=sys.argv[1:]
@@ -26,7 +27,7 @@ for fn in Input_files:
                 err_mom1=fields[4].split(":")[1]
                 ml1=fields[5].split(":")[1]
                 err_ml1=fields[6].split(":")[1]
-                ## Sometimes angsd fails to run method 2, and the error is printed directly after the SE for ML. When that happens, exclude the first word in the error from the output. (Method 2 data will be shown as NA)
+                ## Sometimes angsd fails to run method 2, and the error is printed directly after the SE for ML. When that happens, exclude the first word in the error from the output. (Method 2 jsonOut will be shown as NA)
                 if err_ml1.endswith("contamination"):
                     err_ml1 = err_ml1[:-13]
             elif line.strip()[0:7] == "Method2" and line.strip()[9:16] == 'new_llh':
@@ -37,5 +38,20 @@ for fn in Input_files:
         data[Ind]={ "Number_of_SNPs" : nSNPs, "Method1_MOM_estimate" : mom1, "Method1_MOM_SE" : err_mom1, "Method1_ML_estimate" : ml1, "Method1_ML_SE" : err_ml1, "Method2_MOM_estimate" : mom2, "Method2_MOM_SE" : err_mom2, "Method2_ML_estimate" : ml2, "Method2_ML_SE" : err_ml2 }
         print (Ind, nSNPs, mom1, err_mom1, ml1, err_ml1, mom2, err_mom2, ml2, err_ml2, sep="\t", file=output)
 
+
+jsonOut = {"plot_type": "generalstats", 
+    "pconfig": {
+        "Num_SNPs" : {"title" : "Number of SNPs"},
+        "Method1_MOM_estimate" : {"title": "Contamination Estimate (M1_MOM)"},
+        "Method1_MOM_SE" : {"title": "Estimate Error (M1_MOM)"},
+        "Method1_ML_estimate" : {"title": "Contamination Estimate (M1_ML)"},
+        "Method1_ML_SE" : {"title": "Estimate Error (M1_ML)"},
+        "Method2_MOM_estimate" : {"title": "Contamination Estimate (M2_MOM)"},
+        "Method2_MOM_SE" : {"title": "Estimate Error (M2_MOM)"},
+        "Method2_ML_estimate" : {"title": "Contamination Estimate (M2_ML)"},
+        "Method2_ML_SE" : {"title": "Estimate Error (M2_ML)"}
+    }, 
+    "data" = data
+}
 with open('nuclear_contamination.json', 'w') as outfile:
-    json.dump(data, outfile)
+    json.dump(jsonOut, outfile)
