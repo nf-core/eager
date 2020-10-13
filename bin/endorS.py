da80a377266d56f013a23024f89126b219c691bd
James A. Fellows Yates <jfy133@gmail.com>
2020-10-13 10:06:50 +0200

    Update bin/endorS.py
    Co-authored-by: Aida Andrades Valtueña <aida.andrades@gmail.com>

--- bin/endorS.py
+++ bin/endorS.py
@@ -4,7 +4,7 @@
 # See git repository (https://github.com/aidaanva/endorS.py) for full license text.
 
 """Script to calculate the endogenous DNA in a sample from samtools flag stats.
-It accepts can accept up to two files: pre-quality and post-quality filtering. We recommend
+It can accept up to two files: pre-quality and post-quality filtering. We recommend
 to use both files but you can also use the pre-quality filtering.
 """
 import re
