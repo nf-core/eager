2faa5cee13ff990406e4e8b57d5962b4f26f5235
James Fellows Yates <jfy133@gmail.com>
2020-12-22 12:07:51 +0100

    Fix scrap_software_versions.py

--- bin/scrape_software_versions.py
+++ bin/scrape_software_versions.py
@@ -35,7 +35,7 @@ regexes = {
     'VCF2genome':['v_vcf2genome.txt', r"VCF2Genome \(v. ([0-9].[0-9]+) "],
     'endorS.py':['v_endorSpy.txt', r"endorS.py (\S+)"],
     'kraken':['v_kraken.txt', r"Kraken version (\S+)"],
-    'eigenstrat_snp_coverage':['v_eigenstrat_snp_coverage.txt',r"(\S+)"]
+    'eigenstrat_snp_coverage':['v_eigenstrat_snp_coverage.txt',r"(\S+)"],
     'bbduk':['v_bbduk.txt',r"(\S+)"]
 }
 
