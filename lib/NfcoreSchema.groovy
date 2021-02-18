045734a02b3fe9cf1db49becfdf719649a5dcec5
Alexander Peltzer <alex.peltzer@gmail.com>
2021-02-18 15:20:52 +0100

    Fix for log_colours

--- lib/NfcoreSchema.groovy
+++ lib/NfcoreSchema.groovy
@@ -219,6 +219,23 @@ class NfcoreSchema {
         return params_map
     }
 
+
+    private static Map log_colours(Boolean monochrome_logs) {
+        Map colorcodes = [:]
+        colorcodes['reset']       = monochrome_logs ? '' : "\033[0m"
+        colorcodes['dim']         = monochrome_logs ? '' : "\033[2m"
+        colorcodes['black']       = monochrome_logs ? '' : "\033[0;30m"
+        colorcodes['green']       = monochrome_logs ? '' : "\033[0;32m"
+        colorcodes['yellow']      = monochrome_logs ? '' :  "\033[0;33m"
+        colorcodes['yellow_bold'] = monochrome_logs ? '' : "\033[1;93m"
+        colorcodes['blue']        = monochrome_logs ? '' : "\033[0;34m"
+        colorcodes['purple']      = monochrome_logs ? '' : "\033[0;35m"
+        colorcodes['cyan']        = monochrome_logs ? '' : "\033[0;36m"
+        colorcodes['white']       = monochrome_logs ? '' : "\033[0;37m"
+        colorcodes['red']         = monochrome_logs ? '' : "\033[1;91m"
+        return colorcodes
+    }
+
     static String dashed_line(monochrome_logs) {
         Map colors = log_colours(monochrome_logs)
         return "-${colors.dim}----------------------------------------------------${colors.reset}-"
