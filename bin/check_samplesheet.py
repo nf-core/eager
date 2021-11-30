20434a96c0487113b7b5456ec8355327a66e24e0
James Fellows Yates <jfy133@gmail.com>
2021-11-30 21:58:49 +0100

    Added most applicable checks just need to do file name clashing checks

--- bin/check_samplesheet.py
+++ bin/check_samplesheet.py
@@ -29,9 +29,9 @@ def make_dir(path):
 
 
 def print_error(error, context="Line", context_str=""):
-    error_str = "ERROR: Please check samplesheet -> {}".format(error)
+    error_str = "[check_samplesheet.py] error: Please check samplesheet. {}".format(error)
     if context != "" and context_str != "":
-        error_str = "ERROR: Please check samplesheet -> {}\n{}: '{}'".format(
+        error_str = "[check_samplesheet.py] error: Please check samplesheet. {}\n{}: '{}'".format(
             error, context.strip(), context_str.strip()
         )
     print(error_str)
@@ -42,9 +42,9 @@ def check_samplesheet(file_in, file_out):
     """
     This function checks that the samplesheet follows the following structure:
     sample_id	library_id	lane	colour_chemistry	pairment	strandedness	damage_treatment	r1	r2	bam
-    Sample1	Sample1_Lib1	1	4	PE	double	full	Sample1_Lib1_L008_R1_001.fq.gz	Sample1_Lib1_L008_R2_001.fq.gz	NA
-    Sample2	Sample2_Lib1	2	2	SE	double	full	Sample2_Lib1_L008_R1_001.fq.gz	NA	NA
-    Sample3	Sample3_Lib1	9	4	SE	single	none	NA	NA	Sample3_Lib1.bam
+    Sample1	Sample1_Lib1	1	4	paired	double	full	Sample1_Lib1_L008_R1_001.fq.gz	Sample1_Lib1_L008_R2_001.fq.gz	NA
+    Sample2	Sample2_Lib1	2	2	single	double	full	Sample2_Lib1_L008_R1_001.fq.gz	NA	NA
+    Sample3	Sample3_Lib1	9	4	single	single	none	NA	NA	Sample3_Lib1.bam
 
     For an example see:
     https://github.com/nf-core/test-datasets/raw/eager/testdata/Mammoth/mammoth_design_fastq_bam_dsl2.tsv
@@ -58,7 +58,7 @@ def check_samplesheet(file_in, file_out):
         HEADER = ["sample_id", "library_id", "lane", "colour_chemistry", "pairment", "strandedness", "damage_treatment", "r1", "r2", "bam"]
         header = [x.strip('"') for x in fin.readline().strip().split("\t")]
         if header[: len(HEADER)] != HEADER:
-            print("ERROR: Please check samplesheet header -> {} != {}".format("\t".join(header), "\t".join(HEADER)))
+            print("Please check samplesheet header -> {} != {}".format("\t".join(header), "\t".join(HEADER)))
             sys.exit(1)
 
         ## Check sample entries
@@ -85,58 +85,73 @@ def check_samplesheet(file_in, file_out):
 
             sample_id = sample_id.replace(" ", "_")
             if not sample_id:
-                print_error("[nf-core/eager] error: sample_id entry has not been specified!", "Line", line)
+                print_error("sample_id entry has not been specified!", "Line:", line)
 
             library_id = library_id.replace(" ", "_")
             if not library_id:
-                print_error("[nf-core/eager] error: library_id entry has not been specified!", "Line", line)
+                print_error("library_id entry has not been specified!", "Line:", line)
+
+            if lane.isnumeric() == False:
+                print_error("lane number is not numeric!", "Line:", line)
+
+            if colour_chemistry not in ['2', '4']:
+                print_error("colour_chemistry is not recognised (e.g. 2 for Illumina NextSeq/NovaSeq or 4 Illumina MiSeq/HiSeq/BGI)! Options: 2, 4.", "Line:", line)
+
+            if pairment not in ['paired', 'single']:
+                print_error("pairment is not recognised. Specify single for BAM input. Options: paired, single.", "Line", line)
+
+            if strandedness not in ['double', 'single']:
+                print_error("strandedness is not recognised. Options: double, single", "Line", line)
+
+            if damage_treatment not in ['none', 'half', 'full']:
+                print_error("damage_treatment is not recognised. Corresponds to UDG treatment. Options: none, half, full.", "Line", line)
 
             ## Check input file extensions
             for reads in [r1, r2, bam]:
                 if reads:
                     if reads.find(" ") != -1:
-                        print_error("[nf-core/eager] error: FASTQ or BAM file(s) contains spaces!", "Line", line)
+                        print_error("FASTQ or BAM file(s) contains spaces! Please rename.", "Line", line)
                     if not reads.endswith(".fastq.gz") and not reads.endswith(".fq.gz") and not reads.endswith(".bam") and not reads.endswith("NA"):
                         print_error(
-                            "[nf-core/eager] error: FASTQ or BAM file(s) do not have extension '.fastq.gz', '.fq.gz', or '.bam'!",
+                            "FASTQ or BAM file(s) have unrecognised extension. Options: .fastq.gz, .fq.gz, or .bam!",
                             "Line",
                             line,
                         )
 
-            ## TODO UP TO HERE
-            ## Auto-detect paired-end/single-end
+            ## Prepare meta
             sample_info = []  ## [single_end, fastq_1, fastq_2]
 
-            if sample_id and fastq_1 and fastq_2:  ## Paired-end short reads
-                sample_info = ["0", fastq_1, fastq_2]
-            elif sample and fastq_1 and not fastq_2:  ## Single-end short reads
-                sample_info = ["1", fastq_1, fastq_2]
+            if sample_id and r1 and r2:
+                sample_info = [library_id, lane, colour_chemistry, pairment, strandedness, damage_treatment, r1, r2, bam]
+            elif sample_id and r1 and not r2:
+                sample_info = [library_id, lane, colour_chemistry, pairment, strandedness, damage_treatment, r1, r2, bam]
+            elif sample_id and bam and not r1 and not r2:
+                sample_info = [library_id, lane, colour_chemistry, pairment, strandedness, damage_treatment, r1, r2, bam]
             else:
                 print_error("Invalid combination of columns provided!", "Line", line)
 
-            ## Create sample mapping dictionary = { sample: [ single_end, fastq_1, fastq_2 ] }
-            if sample not in sample_mapping_dict:
-                sample_mapping_dict[sample] = [sample_info]
+            ## Create sample mapping dictionary = { sample: [ library_id, lane, colour_chemistry, pairment, strandedness, damage_treatment, r1, r2, bam ] }
+            if sample_id not in sample_mapping_dict:
+                sample_mapping_dict[sample_id] = [sample_info]
             else:
-                if sample_info in sample_mapping_dict[sample]:
+                if sample_info in sample_mapping_dict[sample_id]:
                     print_error("Samplesheet contains duplicate rows!", "Line", line)
                 else:
-                    sample_mapping_dict[sample].append(sample_info)
+                    sample_mapping_dict[sample_id].append(sample_info)
 
     ## Write validated samplesheet with appropriate columns
     if len(sample_mapping_dict) > 0:
         out_dir = os.path.dirname(file_out)
         make_dir(out_dir)
         with open(file_out, "w") as fout:
-            fout.write(",".join(["sample", "single_end", "fastq_1", "fastq_2"]) + "\n")
-            for sample in sorted(sample_mapping_dict.keys()):
+            fout.write("\t".join(["sample_id", "library_id", "lane", "colour_chemistry", "pairment", "strandedness", "damage_treatment", "r1", "r2", "bam"]) + "\n")
+            for sample_id in sorted(sample_mapping_dict.keys()):
+                ## TODO check that multiple runs of the same sample are of the same datatype [need to improve this, e.g. could be used for checking that SS vs DS libs don't have the same sample name, don't have duplicate lane IDs for same library?]
+                if not all(x[0] == sample_mapping_dict[sample_id][0][0] for x in sample_mapping_dict[sample_id]):
+                    print_error("Libraries of a sample must be of the same strandedness!", "Sample", sample_id)
 
-                ## Check that multiple runs of the same sample are of the same datatype
-                if not all(x[0] == sample_mapping_dict[sample][0][0] for x in sample_mapping_dict[sample]):
-                    print_error("Multiple runs of a sample must be of the same datatype!", "Sample: {}".format(sample))
-
-                for idx, val in enumerate(sample_mapping_dict[sample]):
-                    fout.write(",".join(["{}_T{}".format(sample, idx + 1)] + val) + "\n")
+                for idx, val in enumerate(sample_mapping_dict[sample_id]):
+                    fout.write("\t".join(["{}_T{}".format(sample_id, idx + 1)] + val) + "\n")
     else:
         print_error("No entries to process!", "Samplesheet: {}".format(file_in))
 
